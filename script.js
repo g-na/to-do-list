fb73c200de20a28918fb618a02700ef8fdfec318
G-NA <heo_1325@naver.com>
2023-01-12 16:21:56 +0900

    Update script.js
    localStorage
    JSON

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
 
-const createToDo = function () {
-  const todoList = document.querySelector("#todo-list");
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createToDo = function (storageData) {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+
   const newLi = document.createElement("li");
   const newSpan = document.createElement("span");
   const newBtn = document.createElement("button");
 
   newBtn.addEventListener("click", () => {
     newLi.classList.toggle("complete");
+    saveItemsFn();
   });
 
   newLi.addEventListener("dblclick", () => {
     newLi.remove();
+    saveItemsFn();
   });
 
-  newSpan.textContent = todoInput.value;
+  if (storageData?.complete) {
+    //?  X?.Y; X가 존재하면 X의 Y를 찾는다
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
   newLi.appendChild(newBtn);
   newLi.appendChild(newSpan);
   todoList.appendChild(newLi);
   todoInput.value = "";
+
+  saveItemsFn();
 };
 
 const keyCodeCheck = function () {
@@ -32,4 +48,26 @@ const deleteAll = function () {
   for (let i = 0; i < liList.length; i++) {
     liList[i].remove();
   }
+  saveItemsFn();
 };
+
+const saveItemsFn = function () {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+
+  saveItems.length === 0
+    ? localStorage.removeItem("saved-items")
+    : localStorage.setItem("saved-items", JSON.stringify(saveItems));
+};
+
+if (savedTodoList) {
+  for (let i = 0; i < savedTodoList.length; i++) {
+    createToDo(savedTodoList[i]);
+  }
+}
