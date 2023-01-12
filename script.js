9f41d1e4029ae1a475c83fdf41ed7e474f2be176
G-NA <heo_1325@naver.com>
2023-01-12 14:53:49 +0900

    foundation
    to do list 기본 기능 구현

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const todoInput = document.querySelector("#todo-input");
 
-const keyCodeCheck = function () {
-  //console.log(window.event);
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
-    const todoList = document.querySelector("#todo-list");
-    const newLi = document.createElement("li");
-    const newSpan = document.createElement("span");
+const createToDo = function () {
+  const todoList = document.querySelector("#todo-list");
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
 
-    newSpan.textContent = todoInput.value;
-    newLi.appendChild(newSpan);
-    todoList.appendChild(newLi);
-    todoInput.value = "";
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+  });
+
+  newSpan.textContent = todoInput.value;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+};
+
+const keyCodeCheck = function () {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createToDo();
+  }
+};
+
+const deleteAll = function () {
+  const liList = document.querySelectorAll("li");
+  for (let i = 0; i < liList.length; i++) {
+    liList[i].remove();
   }
 };
